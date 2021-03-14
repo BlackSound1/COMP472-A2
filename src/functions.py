7f8277a4400d91fc866c52b31260e661595c8544
BlackSound1 <matthewr@videotron.ca>
2021-03-13 21:20:21 -0500

    made some small code style changes. Created new functions.py file to put helper functions. Keeps main.py cleaner

---
+++ src/functions.py
@@ -0,0 +1,8 @@
+from src.puzzle_state import PuzzleState
+from random import shuffle
+
+
+def create_random_puzzle(size):
+    random_list = list(range(1, 10))
+    shuffle(random_list)
+    return PuzzleState([random_list[x:x + size] for x in range(0, len(random_list), size)], 0)
