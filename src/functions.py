4be43c279f12a9649ea72f105bdc7004f9e805e7
EvanMateo7 <gevanmateo@gmail.com>
2021-03-14 14:11:55 -0400

    Update func create_random_puzzle to allow creating dynamic size puzzle like 4x4

--- src/functions.py
+++ src/functions.py
@@ -1,8 +1,8 @@
-from src.puzzle_state import PuzzleState
+from puzzle_state import PuzzleState
 from random import shuffle
 
 
 def create_random_puzzle(size):
-    random_list = list(range(1, 10))
+    random_list = list(range(1, size*size+1))
     shuffle(random_list)
     return PuzzleState([random_list[x:x + size] for x in range(0, len(random_list), size)], 0)
