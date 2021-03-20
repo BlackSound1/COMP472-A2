79dc560eb16a8e0815ec0c51318c0ac7bc7ed877
BlackSound1 <matthewr@videotron.ca>
2021-03-20 15:16:18 -0400

    generated 20 puzzles. These puzzles can scale to any size. Wrote them to file. Generated ascending goal state as required by 2.2. Wrote it to file. Properly read these from their files as compatible with existing code. created a function in main to run A* on these 20 puzzles. Commented other code im main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,14 @@ from functions import *
 
 
 def main():
+    create_20_random_puzzles()
+
+    goal_state = read_state(get_goal_state())
+
+    puzzles = get_all_puzzles()
+
+    test_Astar_on_20_puzzles(goal_state, puzzles)
+
     # Tests
     state = PuzzleState(((1, 2, 3), (4, 5, 6), (7, 8, 9)))
     goal = PuzzleState(((2, 1, 3), (9, 6, 4), (7, 8, 5)))
@@ -27,6 +35,24 @@ def main():
         print(heuristic.__name__, "path")
         search_list = PuzzleState.a_star(start_state, goal_state, heuristic)
 
+        for index, state in enumerate(search_list):
+            print(state, state.level)
+
+
+def test_Astar_on_20_puzzles(goal, puzzles):
+    for idx, puzzle in enumerate(puzzles, 0):
+        print("\nPuzzle " + str(idx + 1) + ":\n")
+        # Test A* using Manhattan distance as heuristic
+        start = read_state(puzzle)
+
+        start_state = PuzzleState(start, 0)
+        goal_state = PuzzleState(goal, 0)
+        search_list = PuzzleState.a_star(start_state, goal_state, PuzzleState.manhattan_distance)
+        search_list.reverse()
+
+        print("start:", start_state)
+        print("goal:", goal_state)
+        print("path:")
         for index, state in enumerate(search_list):
             print(state, state.level)
 
