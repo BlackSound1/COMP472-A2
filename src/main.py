a9ec8ae82fa517a03eadf1ec69efe0d2b1336ffc
BlackSound1 <matthewr@videotron.ca>
2021-03-21 20:09:09 -0400

    Generate 20 puzzles (#6)
    * Update func create_random_puzzle to allow creating dynamic size puzzle like 4x4
    
    * Update A* for dynamic size puzzles + update main to test all A* heuristics
    
    * Implement parent state in PuzzleState
    
    * Added code to generate 20 random puzzles. This code scales to any size properly. Writes these puzzles to input/input_puzzles.txt.
    
    * unignored input
    
    * generated 20 puzzles. These puzzles can scale to any size. Wrote them to file. Generated ascending goal state as required by 2.2. Wrote it to file. Properly read these from their files as compatible with existing code. created a function in main to run A* on these 20 puzzles. Commented other code im main.py
    
    * made requested code changes
    
    * Added code to generate 20 random puzzles. This code scales to any size properly. Writes these puzzles to input/input_puzzles.txt.
    
    * unignored input
    
    * generated 20 puzzles. These puzzles can scale to any size. Wrote them to file. Generated ascending goal state as required by 2.2. Wrote it to file. Properly read these from their files as compatible with existing code. created a function in main to run A* on these 20 puzzles. Commented other code im main.py
    
    * made requested code changes
    
    * resolved merge conflicts
    
    Co-authored-by: EvanMateo7 <gevanmateo@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,34 +1,66 @@
 from puzzle_state import *
-from functions import create_random_puzzle
+from functions import *
 
 
 def main():
+    create_20_random_puzzles()
+
+    goal_state = read_state(get_goal_state())
+
+    puzzles = get_all_puzzles()
+
+    test_Astar_on_20_puzzles(goal_state, puzzles)
+
     # Tests
-    state = PuzzleState(((1, 2, 3), (4, 5, 6), (7, 8, 9)))
-    goal = PuzzleState(((2, 1, 3), (9, 6, 4), (7, 8, 5)))
-    assert PuzzleState.hamming_distance(state, goal) == 6
-    assert PuzzleState.manhattan_distance(state, goal) == 10
-    assert PuzzleState.sum_permutation(state, goal) == 10
+    # state = PuzzleState(((1, 2, 3), (4, 5, 6), (7, 8, 9)))
+    # goal = PuzzleState(((2, 1, 3), (9, 6, 4), (7, 8, 5)))
+    # assert PuzzleState.hamming_distance(state, goal) == 6
+    # assert PuzzleState.manhattan_distance(state, goal) == 10
+    # assert PuzzleState.sum_permutation(state, goal) == 10
+    #
+    # state = PuzzleState(((1, 2), (3, 4)))
+    # goal = PuzzleState(((4, 3), (1, 2)))
+    # assert PuzzleState.hamming_distance(state, goal) == 4
+    # assert PuzzleState.manhattan_distance(state, goal) == 6
+    # assert PuzzleState.sum_permutation(state, goal) == 5
+    #
+    # # Test A* using all heuristics
+    # start_state = create_random_puzzle(3)
+    # goal_state = create_random_puzzle(3)
+    # heuristics = [PuzzleState.sum_permutation, PuzzleState.hamming_distance, PuzzleState.manhattan_distance]
+    # print("start:", start_state)
+    # print("goal:", goal_state)
+    #
+    # for heuristic in heuristics:
+    #     print(heuristic.__name__, "path")
+    #     search_list = PuzzleState.a_star(start_state, goal_state, heuristic)
+    #
+    #     for index, state in enumerate(search_list):
+    #         print(state, state.level)
 
-    state = PuzzleState(((1, 2), (3, 4)))
-    goal = PuzzleState(((4, 3), (1, 2)))
-    assert PuzzleState.hamming_distance(state, goal) == 4
-    assert PuzzleState.manhattan_distance(state, goal) == 6
-    assert PuzzleState.sum_permutation(state, goal) == 5
 
-    # Test A* using all heuristics
-    start_state = create_random_puzzle(3)
-    goal_state = create_random_puzzle(3)
-    heuristics = [PuzzleState.sum_permutation, PuzzleState.hamming_distance, PuzzleState.manhattan_distance]
-    print("start:", start_state)
-    print("goal:", goal_state)
+def test_Astar_on_20_puzzles(goal, puzzles):
+    print("------------")
+    print("A* ALGORITHM")
+    print("------------")
 
-    for heuristic in heuristics:
-        print(heuristic.__name__, "path")
-        search_list = PuzzleState.a_star(start_state, goal_state, heuristic)
+    for idx, puzzle in enumerate(puzzles, 0):
+        print("\nPuzzle " + str(idx + 1) + ":\n")
+        # Test A* using Manhattan distance as heuristic
+        start = read_state(puzzle)
 
-        for index, state in enumerate(search_list):
-            print(state, state.level)
+        start_state = PuzzleState(start, 0)
+        goal_state = PuzzleState(goal, 0)
+        heuristics = [PuzzleState.sum_permutation, PuzzleState.hamming_distance, PuzzleState.manhattan_distance]
+        print("start:", start_state)
+        print("goal:", goal_state)
+
+        for heuristic in heuristics:
+            print(heuristic.__name__, "path")
+            search_list = PuzzleState.a_star(start_state, goal_state, heuristic)
+
+            for index, state in enumerate(search_list):
+                print(state, state.level)
 
 
 if __name__ == '__main__':
